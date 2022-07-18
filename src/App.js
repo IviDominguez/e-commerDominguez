2b3155f1e795182dceb175babbd81a522761f890
Ivi Dominguez <ividomin@gmail.com>
2022-07-18 20:16:50 -0300

    complemento: navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logoPoke1 from './logoPoke1.png';
+import './App.css';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,53 @@
+
+const NavBar = () =>{
+    return(
+        <nav className="navbar">
+            <a href="index.html"> 
+                    <img src="/logoPoke.png" alt="logo" className="logo"></img>
+            </a>
+            <div className="items">
+                <a className="link" href="#">Productos</a>
+
+                <a className="link" href="#">150 o más!</a>
+
+                <a className="link" href="#">Contacto</a>
+            </div>
+
+        </nav>
+    )
+
+}
+
+
+export default NavBar;
+
+
+
+
+
+//no me toma los estilos utilizando nav ul li
+
+/* const NavBar = () =>{
+    return(
+        <nav className="navbar">
+
+
+                <a href="index.html"> 
+                    <img src="/logoPoke.png" alt="logo" className="logo" />
+                </a>
+                <ul className="items">
+                    <li>
+                        <a className="link" href="#">Productos</a>
+                    </li>
+                    <li>
+                        <a className="link" href="#">150 o más!</a>
+                    </li>
+                    <li>
+                        <a className="link" href="#">Contacto</a>
+                    </li>
+                </ul>
+
+        </nav>
+    )
+
+}*/
